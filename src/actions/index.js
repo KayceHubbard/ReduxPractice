fcaea2e9740b83efea1fa896ba1c8a9fc3af7889
Kayce Hubbard <kayce.hubbard@gmail.com>
2018-06-25 15:36:09 -0500

    Simple book list application using React and Redux.

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export function selectBook(book) {
+    return {
+        // selectBook is an ActionCreator, it needs to return an action,
+        // an object with a type property.
+
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
+}
